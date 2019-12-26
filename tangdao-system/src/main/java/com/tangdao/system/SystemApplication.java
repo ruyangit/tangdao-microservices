da79e9d008da8b86cef5e8b5fee46b9fe0b10e96
ruyang <ruyangit@163.com>
2019-12-26 16:43:33 +0800

    统一认证

--- tangdao-system/src/main/java/com/tangdao/system/SystemApplication.java
+++ tangdao-system/src/main/java/com/tangdao/system/SystemApplication.java
@@ -2,8 +2,16 @@ package com.tangdao.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SystemApplication {
 
 	public static void main(String[] args) {
