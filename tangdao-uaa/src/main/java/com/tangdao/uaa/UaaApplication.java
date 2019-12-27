1992a2fb1f2dc095ee4e41821a96d4e8ab6dbab7
ruyang <ruyangit@163.com>
2019-12-27 13:18:46 +0800

    认证逻辑

--- tangdao-uaa/src/main/java/com/tangdao/uaa/UaaApplication.java
+++ tangdao-uaa/src/main/java/com/tangdao/uaa/UaaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.tangdao.openfeign")
+@EnableResourceServer
 @RestController
 public class UaaApplication {
 
