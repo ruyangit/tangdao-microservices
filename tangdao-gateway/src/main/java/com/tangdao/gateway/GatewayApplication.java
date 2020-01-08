57cb09397126ca580dbe6eaa54eb2781da537bb2
ruyang <ruyangit@163.com>
2020-01-08 10:14:51 +0800

    处理转发请求

--- tangdao-gateway/src/main/java/com/tangdao/gateway/GatewayApplication.java
+++ tangdao-gateway/src/main/java/com/tangdao/gateway/GatewayApplication.java
@@ -6,6 +6,12 @@ package com.tangdao.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 /**
  * Gateway Service
@@ -14,7 +20,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-//@Import(AdditionalRoutes.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
@@ -29,25 +34,13 @@ public class GatewayApplication {
 //				).build();
 //	}
 
-//	@Bean
-//	@LoadBalanced
-//	public WebClient webClient(final ClientHttpConnector clientHttpConnector) {
-//		return WebClient.builder().clientConnector(clientHttpConnector).build();
-//	}
-//
-//	@Bean
-//	public ClientHttpConnector clientHttpConnector() {
-//		return new ReactorClientHttpConnector(HttpClient.from(TcpClient.newConnection()));
-//	}
-
-//	
-//	@Bean
-//	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-//		return new HiddenHttpMethodFilter() {
-//			@Override
-//			public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-//				return chain.filter(exchange);
-//			}
-//		};
-//	}
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter() {
+			@Override
+			public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+				return chain.filter(exchange);
+			}
+		};
+	}
 }
