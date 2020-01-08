a4eb517e6342f3be618245d58259cc54c0bb214e
ruyang <ruyangit@163.com>
2020-01-08 13:59:24 +0800

    0.1.0

---
+++ tangdao-admin-ui/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    devServer: {
+      proxy: {
+        '/frontend': {
+          target: `${process.env.SW_PROXY_TARGET || "http://127.0.0.1:12800"}`,
+          changeOrigin: true,
+        },
+      },
+    },
+    chainWebpack: config => {
+      const svgRule = config.module.rule('svg');
+      svgRule.uses.clear();
+      svgRule
+        .use('svg-sprite-loader')
+        .loader('svg-sprite-loader')
+        .options({
+          symbolId: '[name]',
+        });
+    },
+  };
