804c1eca2d3a12fff581465126dfdb4df2a3ecd0
ruyang <ruyangit@163.com>
2020-01-10 17:16:43 +0800

    0.1.0

---
+++ tangdao-admin-ui/src/components/dialog/Dialog.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="modal fade effect-scale show" :id="id" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered show" role="document">
+        <div class="modal-content">
+          <div class="modal-body pd-20 pd-sm-30">
+            <button type="button" class="close pos-absolute t-15 r-20" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <slot></slot>
+          </div>
+          <div class="modal-footer">
+              footer
+              <slot name="footer"></slot>
+          </div><!-- modal-footer -->
+        </div><!-- modal-content -->
+      </div><!-- modal-dialog -->
+    </div><!-- modal -->
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class TdDialog extends Vue {
+    @Prop() private id: any;
+}
+</script>
---
+++ tangdao-admin-ui/src/components/dropdown/DropdownItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <a href="javascript:;" class="dropdown-item" :class="{'active': (value === active && active)}" v-html="value"></a>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+@Component({})
+export default class RkDropdownItem extends Vue {
+  @Prop() private active!: string | number;
+  @Prop() private value!: string | number;
+}
+</script>
